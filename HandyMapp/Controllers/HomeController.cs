faf8795e9446647a18fec90f4875a729c4b703d6
Janno Teekens <14067293@student.hhs.nl>
2017-12-04 14:05:05 +0100

    Added Pages: result, select area and way of searching

--- HandyMapp/Controllers/HomeController.cs
+++ HandyMapp/Controllers/HomeController.cs
@@ -58,9 +58,24 @@ namespace HandyMapp.Controllers
             return View();
         }
 
+        public IActionResult WayOfSearching()
+        {
+            return View("~/Views/Home/Buildings/WayOfSearching.cshtml");
+        }
+
         public IActionResult PlaceInput()
         {
             return View("~/Views/Home/Buildings/PlaceInput.cshtml");
+        }
+
+        public IActionResult SelectArea()
+        {
+            return View("~/Views/Home/Buildings/SelectArea.cshtml");
+        }
+
+        public IActionResult PlacesResult()
+        {
+            return View("~/Views/Home/Buildings/PlacesResult.cshtml");
         }
 
         public IActionResult Contact()
--- HandyMapp/wwwroot/js/site.js
+++ HandyMapp/wwwroot/js/site.js
@@ -1,1 +1,8 @@
-﻿
+﻿    $(document).ready(function () {
+        $(':input[type="submit"]').prop('disabled', true);
+        $(':input[type="text"]').keyup(function () {
+            if ($(this).val() != '') {
+                $(':button[type="submit"]').prop("disabled", false);
+            }
+        });
+    });
