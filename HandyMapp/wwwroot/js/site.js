4bc548cf5c36b9433335e047697294a95730e0d7
Janno Teekens <14067293@student.hhs.nl>
2017-12-08 10:19:45 +0100

    Star rating and working on flow search accessible buildings

--- HandyMapp/wwwroot/js/site.js
+++ HandyMapp/wwwroot/js/site.js
@@ -1,8 +1,16 @@
-﻿    $(document).ready(function () {
+﻿    $(function () {
         $(':input[type="submit"]').prop('disabled', true);
         $(':input[type="text"]').keyup(function () {
             if ($(this).val() != '') {
                 $(':button[type="submit"]').prop("disabled", false);
+            }
+        });
+        $("#grade").click(function() {
+            var rating = $("#gradeInput").val()-1;
+            var i;
+            for (i = 0; i <= rating; i++) {
+                $(".rating > path.helft").eq(i).css("fill", "#1370B8");
+                $(".rating > path.midden").eq(i / 2).css("fill", "#1370B8");
             }
         });
     });
