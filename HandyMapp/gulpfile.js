37d6cd4ac63093b3becc28b423dde785ddbfe5c2
Wytze Bruinsma <wytze@wytzejelle.nl>
2017-12-08 12:15:31 +0100

    insert less

--- HandyMapp/gulpfile.js
+++ HandyMapp/gulpfile.js
@@ -1,10 +1,18 @@
-﻿/*
+﻿/// <binding ProjectOpened='less' />
+/*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp');
+var gulp = require("gulp");
 
-gulp.task('default', function () {
-    // place code for your default task here
+less = require("gulp-less");
+
+gulp.task("less", function () {
+    return gulp.src('wwwroot/Styles/main.less')
+        .pipe(less())
+        .pipe(gulp.dest('wwwroot/css'));
 });
+
+
+
