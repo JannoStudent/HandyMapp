0bf57d873616e93b9df9de9a1ba3c1e17d8b5801
TheKingDule <dusjak@live.com>
2017-12-11 20:47:27 +0100

    Adding "Less" to the project. "main.less" is converted/formated to "main.css". This will replace "site.css" when everything is copied into it.

--- HandyMapp/gulpfile.js
+++ HandyMapp/gulpfile.js
@@ -1,10 +1,16 @@
-﻿/*
+/// <binding ProjectOpened='less' />
+/*
 This file is the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 */
 
-var gulp = require('gulp');
 
-gulp.task('default', function () {
-    // place code for your default task here
+var gulp = require("gulp");
+    fs = require("fs"),
+    less = require("gulp-less");
+
+gulp.task("less", function () {
+    return gulp.src('Styles/main.less')
+        .pipe(less())
+        .pipe(gulp.dest('wwwroot/css'));
 });
