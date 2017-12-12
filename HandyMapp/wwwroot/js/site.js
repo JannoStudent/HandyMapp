717caebe0f7d84f25e10841eae074d5bd06beb8c
Janno Teekens <14067293@student.hhs.nl>
2017-12-12 11:32:51 +0100

    Star rating for Bernhard. Maybe work on the scroll part of the places result. Have a nice cristmas and a Happy Newyear!

--- HandyMapp/wwwroot/js/site.js
+++ HandyMapp/wwwroot/js/site.js
@@ -1,53 +1,61 @@
-﻿$(function () {
+﻿$(function() {
     $(':input[type="submit"]').prop('disabled', true);
-    $(':input[type="text"]').keyup(function () {
+    $(':input[type="text"]').keyup(function() {
         if ($(this).val() != '') {
             $(':button[type="submit"]').prop("disabled", false);
         }
     });
-    //$("#grade").click(function() {
-    //var rating = $("#gradeInput").val()-1;
-    var rating = 7;
-    var i;
-    var color = "#2ecc71";
-    for (i = 0; i <= rating; i++) {
-        $(".rating > path.helft").eq(i).css("fill", color);
-        $(".rating > path.midden").eq(i / 2).css("fill", color);
-    };
-    $(".PlaceResult").click(function () {
-        if ($(this).css("height") == "200px") {
-            $(".PlaceResult").animate({
-                height: '200px'
-            }, {queue: false });
-            $(this).animate({
-                height: '455px'
-            }, { queue: false });
-            $(".ImageContainter").animate({
-                height: '110px'
-            }, { queue: false });
-            $(this).find(".ImageContainter").animate({
-                height: '200px'
-            }, { queue: false });
-            $(this).siblings().find(".ImageContainter img").animate({
-                marginTop: '-89'
-            }, { queue: false });
-            $(this).find(".ImageContainter img").animate({
-                marginTop: '-50'
-            }, { queue: false });
-            $(this).find(".DescriptionContainer").slideDown({ queue: false });
-            $(this).find(".btn").fadeIn({ queue: false });
-        } else {
-            $(this).animate({ height: "200px" });
-            $(this).find(".ImageContainter img").animate({
-                marginTop: '-89'
-            }, { queue: false });
-            $(".ImageContainter").animate({
-                height: '110px'
-            }, { queue: false });
-            $(this).find(".DescriptionContainer").slideUp({ queue: false });
-            $(this).find(".btn").fadeOut({ queue: false });
-        }
-        //var offset = $(this).offset().top;
-        //$(this).parent().animate({ scrollTop: offset });
+    $("#grade").click(function() {
+        var rating = $("#gradeInput").val();                            /*Bernhard*/
+        var i;
+        var color = "#2ecc71";
+        for (i = 0; i <= rating-1; i++) {
+            $(".rating > path.helft").eq(i).css("fill", color);
+            $(".rating > path.midden").eq(i / 2).css("fill", color);
+        };                                                              /*Bernhard*/
+        $(".PlaceResult").click(function() {
+            if ($(this).css("height") == "200px") {
+                $(".PlaceResult").animate({
+                        height: '200px'
+                    },
+                    { queue: false });
+                $(this).animate({
+                        height: '455px'
+                    },
+                    { queue: false });
+                $(".ImageContainter").animate({
+                        height: '110px'
+                    },
+                    { queue: false });
+                $(this).find(".ImageContainter").animate({
+                        height: '200px'
+                    },
+                    { queue: false });
+                $(this).siblings().find(".ImageContainter img").animate({
+                        marginTop: '-89'
+                    },
+                    { queue: false });
+                $(this).find(".ImageContainter img").animate({
+                        marginTop: '-50'
+                    },
+                    { queue: false });
+                $(this).find(".DescriptionContainer").slideDown({ queue: false });
+                $(this).find(".btn").fadeIn({ queue: false });
+            } else {
+                $(this).animate({ height: "200px" });
+                $(this).find(".ImageContainter img").animate({
+                        marginTop: '-89'
+                    },
+                    { queue: false });
+                $(".ImageContainter").animate({
+                        height: '110px'
+                    },
+                    { queue: false });
+                $(this).find(".DescriptionContainer").slideUp({ queue: false });
+                $(this).find(".btn").fadeOut({ queue: false });
+            }
+            //var offset = $(this).offset().top;
+            //$(this).parent().animate({ scrollTop: offset });
+        });
     });
 });
