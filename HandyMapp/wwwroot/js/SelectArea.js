854323754650fab47bed5dbac94c28cc96071e43
Janno Teekens <14067293@student.hhs.nl>
2018-01-08 20:43:27 +0100

    Added titles and worked on the select area page

---
+++ HandyMapp/wwwroot/js/SelectArea.js
@@ -0,0 +1,19 @@
+﻿console.log("hallo");
+
+var AreaHover = "";
+var AreaClicked = "";
+
+$("g").mouseenter(function () {
+    AreaHover = $(this).attr("id");
+    $("#wijk").text(AreaHover);
+});
+$("g").mouseleave(function () {
+    $("#wijk").text(AreaClicked);
+});
+$("g").click(function () {
+    AreaClicked = $(this).attr("id");
+    $("#wijk").text(AreaClicked);
+    $("#wijk").parent().animate({ borderColor: "#fff" },"fast");
+    $("#wijk").parent().animate({ borderColor: "#1370B8"});
+});
+
