89c151e46c65650ec8cd7b4e29ebbe9565bf5827
Janno Teekens <14067293@student.hhs.nl>
2018-01-09 13:25:17 +0100

    Building review updating

--- HandyMapp/wwwroot/js/ReviewPage.js
+++ HandyMapp/wwwroot/js/ReviewPage.js
@@ -1,1 +1,26 @@
-﻿
+﻿var questions = [
+    "Are there ramps beside the stairs?",
+    "Are there dorpels?",
+    "Are there elevators?",
+    "Are there wheelchair accessible toilets?",
+    "Are hallways wide enough for wheelchairs?",
+    "Are there loose tiles or floormats?"
+];
+var QuestionCounter = 0;
+
+
+
+$("#further").click(function () {
+    $("#EndReview").fadeOut("slow");
+    $("#EndReview").remove();
+    $("#DetailsButtons").fadeIn("slow");
+    $("#DetailsQuestions").fadeOut("fast");
+    $("#DetailsQuestions").text(questions[QuestionCounter]);
+    $("#DetailsQuestions").fadeIn("fast");
+});
+
+$("#DetailsButtons > div").click(function () {
+    $("#DetailsQuestions").text(questions[QuestionCounter]);
+    QuestionCounter += 1;
+    console.log(QuestionCounter);
+});
