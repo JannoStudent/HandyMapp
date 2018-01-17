e5a7c705f0782e635bd9e948ece658db85ab4646
BernhardGithub <bernhardhartmann0012@gmail.com>
2018-01-17 22:01:29 +0100

    fixing last steps

--- HandyMapp/Controllers/Street_eval_Controller.cs
+++ HandyMapp/Controllers/Street_eval_Controller.cs
@@ -59,8 +59,8 @@ namespace HandyMapp.Controllers
                 model.lng = lng;
                 model.streetname = streetname;
                 HttpContext.Session.SetString("lat", lat);
-                HttpContext.Session.Set<String>("lng", lng);
-                HttpContext.Session.Set<String>("streetname", streetname);
+                HttpContext.Session.SetString("lng", lng);
+                HttpContext.Session.SetString("streetname", streetname);
             }
             return View("street_eval2", model);
         }
@@ -77,41 +77,40 @@ namespace HandyMapp.Controllers
             return View("street_eval3", model);
         }
 
-        street_eval_model model4;
         [HttpPost]
         public IActionResult street_eval4(string myrating)
         {
-            model4 = new street_eval_model();
-            model4.rating = myrating;
+            model = new street_eval_model();
+            model.rating = myrating;
             HttpContext.Session.SetString("rating", JsonConvert.SerializeObject(myrating));
-            return View("street_eval4", model4);
+            return View("street_eval4", model);
         }
         public IActionResult street_eval5(string Obst_description, string selectedObst_type)
         {
-            street_eval_model model4 = new street_eval_model();
-            model4.aid = HttpContext.Session.GetString("aid");
-            model4.lat = HttpContext.Session.Get<String>("lat");
-            model4.lng = HttpContext.Session.Get<String>("lng");
-            model4.streetname = HttpContext.Session.GetString("streetname");
-            model4.rating = HttpContext.Session.GetString("rating");
-            model4.description = Obst_description;
-            model4.obst_type = selectedObst_type;
-            if (model4 != null)
+            street_eval_model model = new street_eval_model();
+            model.aid = HttpContext.Session.GetString("aid");
+            model.lat = HttpContext.Session.Get<String>("lat");
+            model.lng = HttpContext.Session.Get<String>("lng");
+            model.streetname = HttpContext.Session.GetString("streetname");
+            model.rating = HttpContext.Session.GetString("rating");
+            model.description = Obst_description;
+            model.obst_type = selectedObst_type;
+            if (model != null)
             {
                 street_eval_model theModel = new street_eval_model()
                 {
-                    aid = model4.aid,
-                    lat = model4.lat,
-                    lng = model4.lng,
-                    streetname = model4.streetname,
-                    rating = model4.rating,
-                    description = model4.description,
-                    obst_type = model4.obst_type
+                    aid = model.aid,
+                    lat = model.lat,
+                    lng = model.lng,
+                    streetname = model.streetname,
+                    rating = model.rating,
+                    description = model.description,
+                    obst_type = model.obst_type
                 };
                 _context.StreetEvalModels.Add(theModel);
                 _context.SaveChanges();
                 HttpContext.Session.Clear();
-                model4 = new street_eval_model();
+                model = new street_eval_model();
             }
             return View("street_eval5");
         }
