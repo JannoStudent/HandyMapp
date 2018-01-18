0294e3c04444aed972b71c45e27e5ac8e6daec46
Janno Teekens <14067293@student.hhs.nl>
2018-01-18 13:22:28 +0100

    Stars on place details work!

--- HandyMapp/wwwroot/js/PlaceDetails.js
+++ HandyMapp/wwwroot/js/PlaceDetails.js
@@ -5,12 +5,25 @@
         $("#averageRatingStars > path.helft").eq(i).addClass("colorSelected");
         $("#averageRatingStars > path.midden").eq(i / 2).addClass("colorSelected");
     }
-
-    var ratingReview = ($("#reviewRatting").text() * 2); //Variable input         reviewRatting        
-    var b;
-    console.log(ratingReview);
-    for (b = 0; b <= ratingReview - 1; b++) {
-        $("#reviewRattingStars > path.helft").eq(b).addClass("colorSelected");
-        $("#reviewRattingStars > path.midden").eq(b / 2).addClass("colorSelected");
+     //Variable input         reviewRatting  
+    var a;
+    var reviewCounter = $(".ReviewStar").length;
+    for (a = 0; a < reviewCounter; a++) {
+        var starId = "#reviewRatting" + [a];
+        var starHalfId = "#reviewRattingStars" + [a];
+        var ratingReview = ($(starId).text() * 2);
+        console.log(starId);
+        console.log(ratingReview);
+        var b;
+        for (b = 0; b <= ratingReview - 1; b++) {
+            $(starHalfId + "> path.helft").eq(b).addClass("colorSelected");
+            $(starHalfId + "> path.midden").eq(b / 2).addClass("colorSelected");
+        }
     }
+    
+    /*var objectId = "#" + $(this).find(".rating").attr("id");
+    for (i = 0; i <= rating - 1; i++) {
+        $(objectId + "> path.helft").eq(i).addClass("colorSelected");
+        $(objectId + "> path.midden").eq(i / 2).addClass("colorSelected");
+    } */
 });
